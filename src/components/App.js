c3de8df9dfba7d62bfa0888046bbcb1e736e2d44
Jaime Fernández-Castaño Riancho <jaimeferncast@gmail.com>
2022-01-03 14:24:43 +0100

    add timeout to api calls to comply with the daily limit and add companies list

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,118 @@
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react";
 
-import { Container, Form, Input, Header, Label } from 'semantic-ui-react'
+import {
+  Container,
+  Form,
+  Input,
+  Header,
+  Label,
+  Button,
+  Icon,
+  Divider,
+} from "semantic-ui-react";
 
-import AlphaVantageService from '../service/alpha-vantage.service'
+import stocksList from "../data.js";
+import AlphaVantageService from "../service/alpha-vantage.service";
 
 function App() {
+  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [info, setInfo] = useState("");
+  const [error, setError] = useState("");
 
-  const [name, setName] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(false)
-
-  const alphaVantageService = new AlphaVantageService()
+  const alphaVantageService = new AlphaVantageService();
+  const delay = 180000;
 
   const handleInputChange = (e) => {
-    error && setError(false)
-    const { value } = e.target
-    setName(value)
-  }
+    error && setError("");
+    const { value } = e.target;
+    setName(value);
+  };
 
   const submit = async () => {
-    setIsLoading(true)
-    setError(false)
+    setIsLoading(true);
+    setError("");
+    setData(null);
 
-    const response = await alphaVantageService.getRSI(name)
-    if (response.data === {}) setError(true)
+    const response = await alphaVantageService.getRSI(name);
+    if (!response.data["Technical Analysis: RSI"]) setError("Symbol not found");
     else {
-      setData(response.data)
-      setName("")
-      setIsLoading(false)
+      setData(
+        response.data["Technical Analysis: RSI"][
+          Object.keys(response.data["Technical Analysis: RSI"])[0]
+        ]
+      );
+      setName("");
+      setIsLoading(false);
     }
-  }
+  };
+
+  const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    useEffect(() => {
+      savedCallback.current = callback;
+    });
+
+    useEffect(() => {
+      const tick = () => {
+        savedCallback.current();
+      };
+
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  };
+
+  useInterval(
+    () => {
+      if (index === stocksList.length) setIndex(0);
+      else {
+        const code = stocksList[index];
+
+        alphaVantageService.getRSI(code).then((response) => {
+          const value =
+            response.data["Technical Analysis: RSI"][
+              Object.keys(response.data["Technical Analysis: RSI"])[0]
+            ].RSI;
+
+          if (!response.data["Technical Analysis: RSI"]) {
+            setInfo(info + `${code} does not exist` + <br />);
+          } else if (+value < 35) {
+            setInfo(info + `BINGO! ${code} has an RSI of ${value}` + <br />);
+          } else {
+            setInfo(info + `${code}'s RSI is above 35` + <br />);
+          }
+        });
+
+        setIndex(index + 1);
+      }
+    },
+    isTimerRunning ? delay : null
+  );
+
+  const runSearch = () => {
+    setIsTimerRunning(true);
+  };
+
+  const pauseSearch = () => {
+    setIsTimerRunning(false);
+    setIndex(0);
+  };
 
   return (
     <main style={{ paddingTop: 20 }}>
       <Container>
-        <Header as="h1" textAlign="center">Hola Julia</Header>
+        <Header as="h1" textAlign="center">
+          Oversold stocks finder
+        </Header>
         <Form onSubmit={submit}>
           <Form.Field>
             <Input
@@ -50,23 +125,31 @@ function App() {
               value={name}
               onChange={handleInputChange}
             />
-            {error &&
-              <Label basic color='red' pointing>
-                Symbol not found
+            {error && (
+              <Label basic color="red" pointing>
+                {error}
               </Label>
-            }
+            )}
           </Form.Field>
         </Form>
-        {data &&
+        {data && (
           <pre>
-            <code>
-              {JSON.stringify(data, null, 4)}
-            </code>
+            <code>{JSON.stringify(data, null, 4)}</code>
           </pre>
-        }
+        )}
+        <Divider />
+        <Button icon labelPosition="right" color="green" onClick={runSearch}>
+          <Icon name="play" />
+          Run Search
+        </Button>
+        <Button icon labelPosition="left" color="red" onClick={pauseSearch}>
+          <Icon name="pause" />
+          Pause
+        </Button>
+        {info && <p>{info}</p>}
       </Container>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/service/alpha-vantage.service.js
+++ src/service/alpha-vantage.service.js
@@ -1,15 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 
 class AlphaVantageService {
-
   constructor() {
     this.api = axios.create({
-      baseURL: 'https://www.alphavantage.co',
+      baseURL: "https://www.alphavantage.co",
       json: true,
-    })
+    });
   }
 
-  getRSI = (symbol) => this.api.get(`/query?function=RSI&symbol=${symbol}&interval=weekly&time_period=10&series_type=open&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`)
+  getRSI = (symbol) =>
+    this.api.get(
+      `/query?function=RSI&symbol=${symbol}&interval=daily&time_period=14&series_type=close&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`
+    );
 }
 
-export default AlphaVantageService
+export default AlphaVantageService;
